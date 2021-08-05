7f56aaa290c1378cb0ce0a458e0a78d2d1b593ac
Darya Pasuyk <dashka_love@bk.ru>
2021-08-05 08:37:58 +0200

    function haversine to find distance between two points

--- haversine.py
+++ haversine.py
@@ -5,7 +5,7 @@ def haversine(lon1, lat1, lon2, lat2, radius=6371):
     lon2 = np.radians(lon2) 
     dlat = lat2 - lat1 
     dlon = lon2 - lon1 
-    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2 
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2 
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a)) 
     dists = radius * c 
     return dists
